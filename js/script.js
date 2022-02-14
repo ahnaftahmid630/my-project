1b639b516c24584163df84bf1f96917274ba07e2
Tahmid <99605826+ahnaftahmid630@users.noreply.github.com>
2022-02-14 13:32:59 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+$('.sliders').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 500,
+  arrows:true,
+  prevArrow:' <i class="fa-solid fa-arrow-left prev"></i>',
+  nextArrow:'<i class="fa-solid fa-arrow-right next"></i>',
+});
+				
+
+
+});
